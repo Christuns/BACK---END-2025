7faaae4197303cc8083f3fa002fbcb4538eca2e5
lab05l <Christiannascimento179@gmail.com>
2025-08-27 22:08:46 -0300

    PROJECT CONFIGURATION FINISH

---
+++ blog/blog/src/test/java/br/edu/unp/back_end/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unp.back_end.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
